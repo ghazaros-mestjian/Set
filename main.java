6f40d5d5d38ef35fa93213cc581f9c859a0fd6e6
ghazaros-mestjian <ghazaros_mestjian@edu.aua.am>
2026-08-03 16:18:15 +0400

    Testing

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i=0;i<100;i++){
+            if (i%2==0){
+                System.out.println(i+" ");
+            }
+        }
     }
 }
