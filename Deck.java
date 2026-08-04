796ec7ca80722f6c6c774e3591acd1d46dde1e0f
ghazaros-mestjian <ghazaros_mestjian@edu.aua.am>
2026-08-04 18:30:47 +0400

    Testing drawCard()

--- Deck.java
+++ Deck.java
@@ -27,4 +27,12 @@ public class Deck {
         }
         return cards.remove(cards.size() - 1);
     }
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.shuffle();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(deck.drawCard());
+        }
+    }
 }
